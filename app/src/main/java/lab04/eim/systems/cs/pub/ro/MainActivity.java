0b362ac3c962d90545c994126580c66ce747b7e2
zubair farahi <zubair.farahi121@yahoo.com>
2020-03-15 21:44:48 +0200

    lab commit

---
+++ app/src/main/java/lab04/eim/systems/cs/pub/ro/MainActivity.java
@@ -0,0 +1,14 @@
+package lab04.eim.systems.cs.pub.ro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
